5ac6edec1f4583166cfb971f450077f2a32276ff
pognibene <pognibene@53cfe6de-d4c4-44b4-8d26-4b3c076bb653>
2013-06-09 09:34:25 +0000

    added cpp tests for chekc mock order. works.

---
+++ opmock2/samples/cpp_check_mock_order/tests.hpp
@@ -0,0 +1,12 @@
+#ifndef TESTS_HPP_
+#define TESTS_HPP_
+
+void test_mock_1();
+void test_mock_2();
+void test_mock_3();
+void test_mock_4();
+void test_mock_5();
+void test_cmp_ptr_with_typedef();
+void test_cmp_ptr_with_typedef_fail();
+
+#endif
